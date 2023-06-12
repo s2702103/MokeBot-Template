6ee552783fcfd7b05ae3838021a31813fb39972b
Unforetold <123131947+Unforetold@users.noreply.github.com>
2023-06-12 12:37:39 +1000

    Pushin!

--- Index.js
+++ Index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const client = Discord.Client()
 
+
 client.on("ready", () => {
 
     console.log("Ready!")
@@ -23,7 +24,17 @@ client.on("message", message => { // When the bot spots a message
     const command = args.shift().toLowerCase(); // Detect existing commands
 
     //Commands
-    if (command === "ping") { // "!ping"
+    if (command === "test") { // "!ping"
         message.channel.send('Pong!'); // Send a message saying "Pong!"
     }
+    // COPY HERE
+    if (command === "help") { // "!help"
+        const embed = new Discord.MessageEmbed()
+            .setColor("#0099ff")
+            .setTitle("You need help eh?")
+            .setDescription("Here's a life lesson, don't get help - it's smart.")
+
+        message.channel.send(embed);
+    }
+    // TO HERE FOR A CMD.
 });
